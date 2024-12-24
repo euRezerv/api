a4de077c600a2fcf60d26e4e99097b805e49b663
vagrosu <vagrosu@yahoo.com>
2024-12-24 12:53:21 +0200

    Update logger to include stack trace in error logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,15 +2,15 @@ import { createLogger, format, transports } from "winston";
 const { combine, timestamp, printf, colorize } = format;
 import util from "util";
 
-const logFormat = printf(({ level, message, timestamp }) => {
-  return `--> ${timestamp} [${level}]: ${
-    typeof message === "object" ? util.inspect(message, { depth: null, colors: true }) : message
-  }`;
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  const logMessage = typeof message === "object" ? util.inspect(message, { depth: null, colors: true }) : message;
+
+  return `--> ${timestamp} [${level}]: ${logMessage} ${stack ? `\nStack Trace: ${stack}` : ""}`;
 });
 
 const log = createLogger({
   level: "info",
-  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), colorize(), logFormat),
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), colorize(), format.errors({ stack: true }), logFormat),
   transports: [
     new transports.Console(),
     new transports.File({ filename: "logs/errors.log", level: "error" }),
