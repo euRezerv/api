ae81cb723754674e0113739f52dec4f5b337c852
vagrosu <vagrosu@yahoo.com>
2024-12-25 19:53:04 +0200

    Update test workflow

--- scripts/test.sh
+++ scripts/test.sh
@@ -46,8 +46,6 @@ dotenv -e $envFile -- prisma migrate deploy || {
   exit 1;
 }
 
-exit 0
-
 # Run Jest tests with coverage
 echo "Running Jest tests..."
 dotenv -e $envFile -- jest --coverage --detectOpenHandles || {
