30ebbeb21fa953c6d2f1466f1794c4a955504970
vagrosu <vagrosu@yahoo.com>
2025-01-02 22:07:11 +0200

    Add environment variable types for configuration

--- src/globalTypes.d.ts
+++ src/globalTypes.d.ts
@@ -6,6 +6,32 @@ export type CompleteUser = Prisma.User & {
 };
 
 declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      // # Server
+      NODE_ENV: "test" | "development" | "production";
+      PORT: string;
+      URL: string;
+
+      // # Database
+      DATABASE_USER: string;
+      DATABASE_PASSWORD: string;
+      DATABASE_HOST: string;
+      DATABASE_PORT: string;
+      DATABASE_NAME: string;
+      CONNECTION_STRING: string;
+
+      // # Auth
+      SESSION_SECRET: string;
+
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+
+      // # Web
+      WEB_URL: string;
+    }
+  }
+
   namespace Express {
     interface User extends CompleteUser {} // Overwrite the Express.User interface
 
