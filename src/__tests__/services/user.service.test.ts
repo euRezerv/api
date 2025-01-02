5c13d26741aecd64a7d8c7279276c3663b64788c
vagrosu <vagrosu@yahoo.com>
2025-01-02 19:19:41 +0200

    Enhance UserService methods to include deleted users and add createUser functionality

--- src/__tests__/services/user.service.test.ts
+++ src/__tests__/services/user.service.test.ts
@@ -1,5 +1,5 @@
 import UserService from "src/services/user.service";
-import { clearTestDb, createTestUser } from "../testUtils/db";
+import { clearTestDb, createTestUser, getTestGoogleProfileData, getTestLocalProfile } from "../testUtils/db";
 
 describe("UserService", () => {
   beforeEach(async () => {
@@ -28,13 +28,35 @@ describe("UserService", () => {
 
     it("should return null if the user was deleted", async () => {
       // arrange
-      const dbUser = await createTestUser({ deletedAt: new Date() });
+      const dbUser = await createTestUser({ userData: { deletedAt: new Date() } });
 
       // act
       const result = await UserService.getUserById(dbUser.id);
 
       // assert
       expect(result).toBeNull();
+    });
+
+    it("should return the existing user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser();
+
+      // act
+      const result = await UserService.getUserById(dbUser.id, true);
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+
+    it("should return the deleted user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser({ userData: { deletedAt: new Date() } });
+
+      // act
+      const result = await UserService.getUserById(dbUser.id, true);
+
+      // assert
+      expect(result).toEqual(dbUser);
     });
   });
 
@@ -44,7 +66,7 @@ describe("UserService", () => {
       const dbUser = await createTestUser();
 
       // act
-      const result = await UserService.getUserByEmail(dbUser.email);
+      const result = await UserService.getUserByEmail(dbUser.localProfile!.email);
 
       // assert
       expect(result).toEqual(dbUser);
@@ -60,13 +82,35 @@ describe("UserService", () => {
 
     it("should return null if the user was deleted", async () => {
       // arrange
-      const dbUser = await createTestUser({ deletedAt: new Date() });
+      const dbUser = await createTestUser({ userData: { deletedAt: new Date() } });
 
       // act
-      const result = await UserService.getUserByEmail(dbUser.email);
+      const result = await UserService.getUserByEmail(dbUser.localProfile!.email);
 
       // assert
       expect(result).toBeNull();
+    });
+
+    it("should return the existing user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser();
+
+      // act
+      const result = await UserService.getUserByEmail(dbUser.localProfile!.email, true);
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+
+    it("should return the deleted user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser({ userData: { deletedAt: new Date() } });
+
+      // act
+      const result = await UserService.getUserByEmail(dbUser.localProfile!.email, true);
+
+      // assert
+      expect(result).toEqual(dbUser);
     });
   });
 
@@ -76,7 +120,10 @@ describe("UserService", () => {
       const dbUser = await createTestUser();
 
       // act
-      const result = await UserService.getUserByPhoneNumber(dbUser.phoneNumber, dbUser.phoneNumberCountryISO);
+      const result = await UserService.getUserByPhoneNumber(
+        dbUser.localProfile!.phoneNumber,
+        dbUser.localProfile!.phoneNumberCountryISO
+      );
 
       // assert
       expect(result).toEqual(dbUser);
@@ -92,13 +139,190 @@ describe("UserService", () => {
 
     it("should return null if the user was deleted", async () => {
       // arrange
-      const dbUser = await createTestUser({ deletedAt: new Date() });
+      const dbUser = await createTestUser({ userData: { deletedAt: new Date() } });
 
       // act
-      const result = await UserService.getUserByPhoneNumber(dbUser.phoneNumber, dbUser.phoneNumberCountryISO);
+      const result = await UserService.getUserByPhoneNumber(
+        dbUser.localProfile!.phoneNumber,
+        dbUser.localProfile!.phoneNumberCountryISO
+      );
 
       // assert
       expect(result).toBeNull();
+    });
+
+    it("should return the existing user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser();
+
+      // act
+      const result = await UserService.getUserByPhoneNumber(
+        dbUser.localProfile!.phoneNumber,
+        dbUser.localProfile!.phoneNumberCountryISO,
+        true
+      );
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+
+    it("should return the deleted user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser({ userData: { deletedAt: new Date() } });
+
+      // act
+      const result = await UserService.getUserByPhoneNumber(
+        dbUser.localProfile!.phoneNumber,
+        dbUser.localProfile!.phoneNumberCountryISO,
+        true
+      );
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+  });
+
+  describe("getUserByGoogleId", () => {
+    it("should return the user with the given google id", async () => {
+      // arrange
+      const dbUser = await createTestUser({ googleProfileData: getTestGoogleProfileData() });
+
+      // act
+      const result = await UserService.getUserByGoogleId(dbUser.googleProfile!.googleId);
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+
+    it("should return null if the user does not exist", async () => {
+      // act
+      const result = await UserService.getUserByGoogleId("non-existing-google-id");
+
+      // assert
+      expect(result).toBeNull();
+    });
+
+    it("should return null if the user was deleted", async () => {
+      // arrange
+      const dbUser = await createTestUser({
+        googleProfileData: getTestGoogleProfileData(),
+        userData: { deletedAt: new Date() },
+      });
+
+      // act
+      const result = await UserService.getUserByGoogleId(dbUser.googleProfile!.googleId);
+
+      // assert
+      expect(result).toBeNull();
+    });
+
+    it("should return the existing user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser({ googleProfileData: getTestGoogleProfileData() });
+
+      // act
+      const result = await UserService.getUserByGoogleId(dbUser.googleProfile!.googleId, true);
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+
+    it("should return the deleted user if specified to include soft deleted users", async () => {
+      // arrange
+      const dbUser = await createTestUser({
+        googleProfileData: getTestGoogleProfileData(),
+        userData: { deletedAt: new Date() },
+      });
+
+      // act
+      const result = await UserService.getUserByGoogleId(dbUser.googleProfile!.googleId, true);
+
+      // assert
+      expect(result).toEqual(dbUser);
+    });
+  });
+
+  describe("createUser", () => {
+    it("should create a new empty user", async () => {
+      // act
+      const result = await UserService.createUser({});
+
+      // assert
+      expect(result).toMatchObject({
+        id: expect.any(String),
+        localProfile: null,
+        googleProfile: null,
+        deletedAt: null,
+      });
+    });
+
+    it("should create a new user with provided data", async () => {
+      // arrange
+      const userData = {
+        createdAt: new Date("2025-01-02"),
+        updatedAt: new Date("2025-01-03"),
+        deletedAt: new Date("2025-01-03"),
+      };
+
+      // act
+      const result = await UserService.createUser({ userData });
+
+      // assert
+      expect(result).toMatchObject({
+        id: expect.any(String),
+        localProfile: null,
+        googleProfile: null,
+        ...userData,
+      });
+    });
+
+    it("should create a new user with provided local profile", async () => {
+      // arrange
+      const localProfileData = (await getTestLocalProfile()).data;
+
+      // act
+      const result = await UserService.createUser({ localProfileData });
+
+      // assert
+      expect(result).toMatchObject({
+        id: expect.any(String),
+        localProfile: localProfileData,
+        googleProfile: null,
+        deletedAt: null,
+      });
+    });
+
+    it("should create a new user with provided google profile", async () => {
+      // arrange
+      const googleProfileData = getTestGoogleProfileData();
+
+      // act
+      const result = await UserService.createUser({ googleProfileData });
+
+      // assert
+      expect(result).toMatchObject({
+        id: expect.any(String),
+        localProfile: null,
+        googleProfile: googleProfileData,
+        deletedAt: null,
+      });
+    });
+
+    it("should create a new user with provided local and google profiles", async () => {
+      // arrange
+      const localProfileData = (await getTestLocalProfile()).data;
+      const googleProfileData = getTestGoogleProfileData();
+
+      // act
+      const result = await UserService.createUser({ localProfileData, googleProfileData });
+
+      // assert
+      expect(result).toMatchObject({
+        id: expect.any(String),
+        localProfile: localProfileData,
+        googleProfile: googleProfileData,
+        deletedAt: null,
+      });
     });
   });
 });
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,36 +1,91 @@
+import { Prisma } from "@prisma/client";
 import { normalizeError } from "@toolbox/common/errors";
 import log from "@utils/logger";
 import prisma from "@utils/prisma";
 
+type CreateUserProps = {
+  userData?: Omit<Prisma.UserCreateInput, "localProfile" | "googleProfile">;
+  localProfileData?: Omit<Prisma.LocalProfileCreateInput, "user">;
+  googleProfileData?: Omit<Prisma.GoogleProfileCreateInput, "user">;
+};
+
 export default class UserService {
-  static async getUserById(userId: string) {
-    try {
-      return await prisma.user.findUnique({ where: { id: userId, deletedAt: null } });
-    } catch (error) {
-      log.error(error);
-      throw normalizeError(error);
-    }
-  }
-
-  static async getUserByEmail(email: string) {
-    try {
-      return await prisma.user.findUnique({ where: { email, deletedAt: null } });
-    } catch (error) {
-      log.error(error);
-      throw normalizeError(error);
-    }
-  }
-
-  static async getUserByPhoneNumber(phoneNumber: string, phoneNumberCountryISO: string) {
+  static async getUserById(userId: string, includeDeleted = false) {
     try {
       return await prisma.user.findUnique({
+        where: { id: userId, deletedAt: includeDeleted ? undefined : null },
+        include: { localProfile: true, googleProfile: true },
+      });
+    } catch (error) {
+      log.error(error);
+      throw normalizeError(error);
+    }
+  }
+
+  static async getUserByEmail(email: string, includeDeleted = false) {
+    try {
+      return await prisma.user.findFirst({
         where: {
-          phoneNumberCountryISO_phoneNumber: {
+          localProfile: {
+            email,
+          },
+          deletedAt: includeDeleted ? undefined : null,
+        },
+        include: { googleProfile: true, localProfile: true },
+      });
+    } catch (error) {
+      log.error(error);
+      throw normalizeError(error);
+    }
+  }
+
+  static async getUserByPhoneNumber(phoneNumber: string, phoneNumberCountryISO: string, includeDeleted = false) {
+    try {
+      return await prisma.user.findFirst({
+        where: {
+          localProfile: {
             phoneNumberCountryISO,
             phoneNumber,
           },
-          deletedAt: null,
+          deletedAt: includeDeleted ? undefined : null,
         },
+        include: { googleProfile: true, localProfile: true },
+      });
+    } catch (error) {
+      log.error(error);
+      throw normalizeError(error);
+    }
+  }
+
+  static async getUserByGoogleId(googleId: string, includeDeleted = false) {
+    try {
+      return await prisma.user.findFirst({
+        where: {
+          googleProfile: {
+            googleId,
+          },
+          deletedAt: includeDeleted ? undefined : null,
+        },
+        include: {
+          googleProfile: true,
+          localProfile: true,
+        },
+      });
+    } catch (error) {
+      log.error(error);
+      throw normalizeError(error);
+    }
+  }
+
+  static async createUser({ userData, localProfileData, googleProfileData }: CreateUserProps) {
+    try {
+      return await prisma.user.create({
+        data: {
+          ...userData,
+          localProfile: localProfileData ? { create: localProfileData } : undefined,
+          googleProfile: googleProfileData ? { create: googleProfileData } : undefined,
+        },
+        include: { localProfile: true, googleProfile: true },
       });
     } catch (error) {
       log.error(error);
