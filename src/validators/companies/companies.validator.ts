62600c86026fef7c2469ac4a124a650710d26ecc
vagrosu <vagrosu@yahoo.com>
2025-01-12 17:58:59 +0200

    Refactor company validation logic and move invitation validation to a new file

--- src/validators/companies/companies.validator.ts
+++ src/validators/companies/companies.validator.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { check, param, query } from "express-validator";
-import validationResultFormatter from "./validationResultFormatter";
+import validationResultFormatter from "../validationResultFormatter";
 import { isObjectEmpty } from "@toolbox/common/objects";
 import { standardResponse } from "@utils/responses";
 import { CompanyEmployeeRole } from "@prisma/client";
@@ -107,51 +107,6 @@ export const validateCreateCompany: RequestHandler[] = [
     .withMessage("Longitude must be a number")
     .isFloat()
     .withMessage("Longitude must be a number"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResultFormatter(req);
-    if (!isObjectEmpty(errors)) {
-      res.status(400).json(standardResponse({ isSuccess: false, res, message: "Validation error", errors }));
-      return;
-    }
-
-    next();
-  },
-];
-
-export const validateInviteEmployeeToCompany: RequestHandler[] = [
-  param("companyId")
-    .not()
-    .isEmpty({ ignore_whitespace: true })
-    .withMessage("Company ID is required")
-    .isString()
-    .withMessage("Company ID must be a string"),
-  check("invitedUserId")
-    .not()
-    .isEmpty({ ignore_whitespace: true })
-    .withMessage("Invited user ID is required")
-    .isString()
-    .withMessage("Invited user ID must be a string")
-    .custom((value, { req }) => {
-      if (value === req.user.id) {
-        throw new Error("You cannot invite yourself to a company");
-      }
-
-      return true;
-    }),
-  check("role")
-    .not()
-    .isEmpty({ ignore_whitespace: true })
-    .withMessage("Role is required")
-    .isString()
-    .withMessage("Role must be a string")
-    .custom((value) => {
-      if (!Object.values(CompanyEmployeeRole).includes(value.toUpperCase() as CompanyEmployeeRole)) {
-        throw new Error(`Invalid role. Must be one of: ${Object.values(CompanyEmployeeRole).join(", ")}`);
-      }
-
-      return true;
-    })
-    .toUpperCase(),
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResultFormatter(req);
     if (!isObjectEmpty(errors)) {
